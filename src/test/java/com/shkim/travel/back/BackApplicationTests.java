98a09902aa13fd33deaccdb5e247005ace139d44
SangHyunKim <ksh9891@gmail.com>
2022-02-22 14:12:55 +0900

    init

---
+++ src/test/java/com/shkim/travel/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shkim.travel.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
